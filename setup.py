08d63b5d5126a40f5e60832a5035c66bf3767344
Sébastien Weber <seba.weber@gmail.com>
2023-01-30 14:17:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setup(
     packages=find_packages(where='./src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}'},
+    entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}',
+                  'pymodaq.pid_models': f"{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}"},
     install_requires=['toml', ]+config['plugin-install']['packages-required'],
     **setupOpts
 )

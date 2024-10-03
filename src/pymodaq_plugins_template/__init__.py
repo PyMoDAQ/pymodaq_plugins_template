c2a20364d514f0c01929f61ed8516cbab24618ab
Sébastien Weber <seba.weber@gmail.com>
2024-10-03 23:05:22 +0200

    proper configuration

--- src/pymodaq_plugins_template/__init__.py
+++ src/pymodaq_plugins_template/__init__.py
@@ -1,8 +1,12 @@
 from pathlib import Path
-from pymodaq_utils.logger import set_logger  # to be imported by other modules.
-from _version import __version__, __version_tuple__, version_tuple, version
-
 from .utils import Config
+from pymodaq_utils.utils import get_version, PackageNotFoundError
+
 config = Config()
+try:
+    __version__ = get_version(__package__)
+except PackageNotFoundError:
+    __version__ = '0.0.0dev'
+
 
 
